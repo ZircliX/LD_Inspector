f6c7804c5a30e5b09553b599ed9b3ef6a481f291
ZircliX <lolofy71@gmail.com>
2024-12-03 15:14:31 +0100

    jsp tkt

--- Assets/Scripts/Core/GameController.cs
+++ Assets/Scripts/Core/GameController.cs
@@ -8,24 +8,26 @@ namespace CyberEnigma.Core
         //initialisé les properties
         public static PrioritisedProperty<bool> CursorVisibility { get; private set; }
         public static PrioritisedProperty<CursorLockMode> CursorLockMode { get; private set; }
+        
         //Code exécuter avant le LoadScene
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         private static void Load()
         {
             //Creation
             CursorVisibility = new PrioritisedProperty<bool>(true);
-
             CursorLockMode = new PrioritisedProperty<CursorLockMode>(UnityEngine.CursorLockMode.None);
 
             //Notification quand la valeur change
             CursorVisibility.AddOnValueChangeCallback(UpdateCursorVisibility, true);
             CursorLockMode.AddOnValueChangeCallback(UpdateCursorLockMode, true);
         }
+        
         //Fonction pour le UpdateCursorLockMode
         private static void UpdateCursorLockMode(CursorLockMode lockMode)
         {
             Cursor.lockState = lockMode;
         }
+        
         //Fonction pour le UpdateCursorVisibility
         private static void UpdateCursorVisibility(bool isVisible)
         {
